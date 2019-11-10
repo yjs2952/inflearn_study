027308a91b634f535336f55d511431b64a479840
yjs2952 <yjs2952@naver.com>
2019-11-10 12:37:53 +0900

    jpa 스터디 jpql dto 로 조회하기

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -21,15 +21,37 @@ public class JpaMain {
             member1.setAge(10);
             em.persist(member1);
 
+            em.flush();
+            em.clear();
+
             List<Member> resultList = em.createQuery("select m from Member m where m.username = :username", Member.class)
                     .setParameter("username", "member1")
+                    .getResultList();   // 얘네도 엔티티 매니저가 관리해준다.
+
+            List<Team> result = em.createQuery("select t from Member m join m.team t", Team.class)
+                    .getResultList();   // join 은 명시적으로 표기하는 것이 좋다.
+
+            /*List resultList1 = em.createQuery("select m.username, m.age from Member m")
                     .getResultList();
 
-            //em.flush();
-            //em.clear();
+            Object o = resultList1.get(0);
+            Object[] result1 = (Object[]) o;
+            System.out.println("username : " + result1[0]);
+            System.out.println("age : " + result1[1]);*/
 
-            //Member m1 = em.getReference(Member.class, member1.getId());
-            //System.out.println("m1 = " + m1.getClass());
+            /*List<Object[]> resultList2 = em.createQuery("select m.username, m.age from Member m")
+                    .getResultList();
+
+            Object[] result2 = resultList2.get(0);
+            System.out.println("username : " + result2[0]);
+            System.out.println("age : " + result2[1]);*/
+
+            /*List<MemberDTO> resultList1 = em.createQuery("select new hellojpa.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+
+            MemberDTO memberDTO = resultList1.get(0);
+            System.out.println("memberDTO = " + memberDTO.getUsername());
+            System.out.println("memberDTO = " + memberDTO.getAge());*/
 
             tx.commit();
         } catch (Exception e) {
