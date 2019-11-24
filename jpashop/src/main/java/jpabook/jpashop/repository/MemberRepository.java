6a72f29dbf0647f2e45a6c014db10eaa9fb62085
yjs2952 <yjs2952@naver.com>
2019-11-24 17:14:10 +0900

    jpa 활용편 1 - 테스트코드 작성

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+//@RequiredArgsConstructor
+public class MemberRepository {
+
+    @PersistenceContext           // 원래는 엔티티 매니저를 주입받기 위해선 이 어노테이션을 써야한다. 그러나 스프링부트에서는 @Autowired 로 주입이 가능하다.
+    private EntityManager em; // @Autowired 로 주입 가능한 빈만 이런식으로 생략이 가능하다.
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name= :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
