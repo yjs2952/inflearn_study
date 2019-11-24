0b93f11c3ce082ba6c70fb985f9d00d487f49fd5
yjs2952 <yjs2952@naver.com>
2019-11-24 17:24:19 +0900

    jpa 활용편 1 - 테스트코드 작성1

--- jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -9,11 +9,11 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
 public class MemberRepository {
 
     @PersistenceContext           // 원래는 엔티티 매니저를 주입받기 위해선 이 어노테이션을 써야한다. 그러나 스프링부트에서는 @Autowired 로 주입이 가능하다.
-    private EntityManager em; // @Autowired 로 주입 가능한 빈만 이런식으로 생략이 가능하다.
+    private final EntityManager em; // @Autowired 로 주입 가능한 빈만 이런식으로 생략이 가능하다.
 
     public void save(Member member) {
         em.persist(member);
