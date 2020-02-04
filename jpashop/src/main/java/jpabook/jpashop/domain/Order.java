910ff2e672675d335470f3ed68a3278e55501604
yjs2952 <yjs2952@naver.com>
2020-02-04 23:45:31 +0900

    불필요한 코드 제거

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -3,7 +3,6 @@ package jpabook.jpashop.domain;
 import lombok.Generated;
 import lombok.Getter;
 import lombok.Setter;
-import org.omg.CORBA.portable.Delegate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
