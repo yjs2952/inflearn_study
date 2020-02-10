63dc1aea6dc3dd17007c626e7e0d072d2aac842c
yjs2952 <yjs2952@naver.com>
2020-02-11 00:22:19 +0900

    멤버 insert 테스트

--- jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -11,8 +11,8 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringRunner.class)        // Junit을 스프링에서 사용하기 위한 러너
+@SpringBootTest                     // 스프링 컨테이너 내에서 테스트를 실행하기 위한 어노테이션
 @Transactional
 public class MemberServiceTest {
 
@@ -20,32 +20,33 @@ public class MemberServiceTest {
     @Autowired private MemberRepository memberRepository;
 
     @Test
+    @Rollback(false)
     public void 회원가입() throws Exception {
-        //given
+        // given
         Member member = new Member();
         member.setName("kim");
 
-        //when
+        // when
         Long savedId = memberService.join(member);
 
-        //then
-        Assert.assertEquals(member, memberService.findOne(savedId));
+        // then
+        Assert.assertEquals(member, memberRepository.findOne(savedId));
     }
 
-    @Test//(expected = IllegalStateException.class)
+    @Test(expected = IllegalStateException.class)
     public void 중복_회원_예외() throws Exception {
-        //given
+        // given
         Member member1 = new Member();
         member1.setName("kim1");
 
         Member member2 = new Member();
         member2.setName("kim1");
 
-        //when
+        // when
         memberService.join(member1);
-        memberService.join(member2);    // 예외가 발생해야한다.
+        memberService.join(member2);    // 예외가 발생해야 한다.
 
-        //then
-        Assert.fail("예외가 발생해야 한다.");
+        // then
+        Assert.fail("예외가 발생해야 합니다.");
     }
 }
